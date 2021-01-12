68a1409735d3f997a209165a1cbb01b65c7dbd6e
elinoza <hillcakmak@gmail.com>
2021-01-12 18:39:53 +0300

    all done

---
+++ src/services/articles/schema.js
@@ -0,0 +1,39 @@
+const { Schema } = require("mongoose")
+const mongoose = require("mongoose")
+const mongoosePaginate = require("mongoose-paginate-v2")
+
+const articleSchema = new Schema(
+  {
+    headLine: {
+      type: String,
+      required: true,
+    },
+    subHead: {
+      type: String
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    category:{
+      name:{
+      type: String},
+      img:{
+        type: String}
+      }
+      ,
+      author:{
+        name:{
+        type: String},
+        img:{
+          type: String}
+        }
+        ,
+    cover: {
+      type: String }
+    }
+)
+
+articleSchema.plugin(mongoosePaginate)
+
+module.exports = mongoose.model("article", articleSchema)
